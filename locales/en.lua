6125005740983bf061dfbd71386d1568e65e925b
ibunny99 <103625205+khaineedhv@users.noreply.github.com>
2022-06-26 22:23:29 -0700

    edit vehiclekey

--- locales/en.lua
+++ locales/en.lua
@@ -1,32 +1,32 @@
 local Translations = {
     error = {
-        no_deposit = "$%{value} Deposit Required",
-        cancelled = "Cancelled",
-        vehicle_not_correct = "This is not a commercial vehicle!",
-        no_driver = "You must be the driver to do this..",
-        no_work_done = "You haven't done any work yet..",
+        no_deposit = "$%{value} Tiền đặt cọc yêu cầu",
+        cancelled = "Đã hủy",
+        vehicle_not_correct = "Đây không phải là một chiếc xe thương mại!",
+        no_driver = "Bạn phải là người lái xe để làm điều đó..",
+        no_work_done = "Bạn chưa làm bất kỳ công việc nào..",
     },
     success = {
-        paid_with_cash = "$%{value} Deposit Paid With Cash",
-        paid_with_bank = "$%{value} Deposit Paid From Bank",
-        refund_to_cash = "$%{value} Deposit Paid With Cash",
-        you_earned = "You Earned $%{value}",
-        payslip_time = "You Went To All The Shops .. Time For Your Payslip!",
+        paid_with_cash = "$%{value} Tiền gửi thanh toán bằng tiền mặt",
+        paid_with_bank = "$%{value} Gửi tiền trả từ ngân hàng",
+        refund_to_cash = "$%{value} Tiền gửi thanh toán bằng tiền mặt",
+        you_earned = "Bạn đã kiếm được $%{value}",
+        payslip_time = "Bạn đã hoàn thành công việc ... Đã đến lúc nhận tiền lương của bạn",
     },
     menu = {
-        header = "Available Trucks",
-        close_menu = "⬅ Close Menu",
+        header = "Xe tải có sẵn",
+        close_menu = "⬅ Đóng Menu",
     },
     mission = {
-        store_reached = "Store reached, get a box in the trunk with [E] and deliver to marker",
-        take_box = "Take A Box Of Products",
-        deliver_box = "Deliver Box Of Products",
-        another_box = "Get another Box Of Products",
-        goto_next_point = "You Have Delivered All Products, To The Next Point",
+        store_reached = "Đã đến cửa hàng, lấy một chiếc hộp trong cốp có [E] và giao cho điểm đánh dấu",
+        take_box = "Lấy một hộp sản phẩm",
+        deliver_box = "Cung cấp Hộp Sản phẩm",
+        another_box = "Nhận một hộp sản phẩm khác",
+        goto_next_point = "Bạn đã giao tất cả các sản phẩm, đến điểm tiếp theo",
     },
     info = {
-        deliver_e = "~g~E~w~ - Deliver Products",
-        deliver = "Deliver Products",
+        deliver_e = "~g~E~w~ - Cung cấp sản phẩm",
+        deliver = "Cung cấp sản phẩm",
     }
 }
 
